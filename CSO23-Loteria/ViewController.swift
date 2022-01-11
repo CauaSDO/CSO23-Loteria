1e33b540d128c10e68c1f4fc61f48b9603e0a490
CauaSDO <cauasilva@icloud.com>
2022-01-11 16:33:43 -0300

    Initial Commit

---
+++ CSO23-Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSO23-Loteria
+//
+//  Created by caua on 11/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
